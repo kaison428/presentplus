6df9096438b86d2e50212d259561a839a39b39a6
Kaison <cheungsiukaison.soniacheung@gmail.com>
2023-06-16 03:15:23 -0400

    created app

---
+++ app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.colored_header import colored_header
+from streamlit_extras.add_vertical_space import add_vertical_space
+from hugchat import hugchat
+
+
+import PyPDF2
+
+from model import *
+
+################################################################
+PROMPT_TEMPLATE = '''
+    You are a professional in marketing, specializing in market research and technology development. \
+    You are a judge of an undergraduate marketing presentation competition. \
+    
+    Write a concise summary of the following:\
+    
+    {text}
+
+   The summary should focus on the technical content and technology aspect of presentation.\
+
+'''
+
+st.set_page_config(page_title="PresentPlus - An LLM-powered Presentation Mentor", page_icon="⭐")
+
+# Sidebar contents
+with st.sidebar:
+    st.title('👁️‍🗨️ PresentPlus')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered presentation analyzer built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/en/latest/)
+    
+    ''')
+
+    add_vertical_space(5)
+    st.write('Made by Kaison from HYPE AI')
+
+# Layout of input/response containers
+input_container = st.container()
+# context_container = st.container()
+colored_header(label='', description='', color_name='blue-30')
+response_container = st.container()
+
+# Flag
+isPDF = False
+
+# User input
+## Function for taking user provided PDF as input
+def get_file(key):
+    uploaded_file = st.file_uploader(f"Upload your {key} file", type='pdf', key=key)
+    if uploaded_file is None:
+        st.session_state["upload_state"] = False
+    else:
+        st.session_state["upload_state"] = True
+        
+    return uploaded_file
+
+## Applying the user input box
+with input_container:
+    user_input = get_file('presentation')
+
+# ## Applying the user context box
+# with context_container:
+#     user_context = get_file('context')
+
+# Response output
+## Function for taking user prompt as input followed by producing AI generated responses
+def generate_response(fileobj):
+    text = get_text_from_pdf(fileobj)
+    summary = custom_prompt_summary(text, PROMPT_TEMPLATE, chain_type='map_reduce')
+    return summary
+
+## Conditional display of AI generated responses as a function of user provided prompts
+with response_container:
+    summary = ''
+
+    if user_input:
+        summary = generate_response(user_input)
+        
+    message(summary)
---
+++ model.py
@@ -0,0 +1,60 @@
+import os
+import openai
+import sys
+import utils
+
+import json
+
+# parser
+import PyPDF2
+
+# LangChain
+from langchain import OpenAI, PromptTemplate, LLMChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.mapreduce import MapReduceChain
+from langchain.prompts import PromptTemplate
+
+from langchain.docstore.document import Document
+
+from langchain.chains.summarize import load_summarize_chain
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv(), override=True) # read local .env file
+
+openai.api_key  = os.environ['OPENAI_API_KEY']
+
+def get_text_from_pdf(fileobj):
+
+    #create reader variable that will read the pdffileobj
+    reader = PyPDF2.PdfReader(fileobj)
+    
+    #This will store the number of pages of this pdf file
+    num_pages = len(reader.pages)
+    
+    presentation_text = ''
+    for i in range(num_pages):
+        #create a variable that will select the selected number of pages
+        pageobj = reader.pages[i]
+        text = pageobj.extract_text()
+        presentation_text += '### Page {} {}'.format(i+1, text)
+
+    return presentation_text
+
+def custom_prompt_summary(input, prompt_template, chain_type='map_reduce'):
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+    llm = OpenAI(temperature=0)
+    text_splitter = CharacterTextSplitter()
+
+    texts = text_splitter.split_text(input)
+    docs = [Document(page_content=t) for t in texts[:3]]
+
+    chain = load_summarize_chain(llm, chain_type=chain_type)
+    summary = chain.run(docs)
+    chain = load_summarize_chain(OpenAI(temperature=0)
+                                , chain_type=chain_type
+                                , map_prompt=PROMPT
+                                , combine_prompt=PROMPT)
+    summary = chain.run(docs)
+    
+    return summary
